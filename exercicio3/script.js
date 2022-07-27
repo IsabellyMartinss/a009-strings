ac823b0a06efe7ec1770820dc4c2e87c11d62c20
Isabelly Martins <isabellyksml@gmail.com>
2022-07-27 12:00:22 -0300

    Exercício-Extra3-resolvido

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -3,12 +3,10 @@ const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOA
 const frase2 = frase.replace("verde","rosa").replace("azul","laranja");
 console.log (frase2);
 
-const temGreen = frase2.includes("verde")
-const temOrange = frase2.includes("laranja")
-console.log(`A frase acima contém a palavra "verde"?`)
-console.log(`A frase acima contém a palavra "laranja"?`)
+const temGreen = frase2.includes("verde");
+const temOrange = frase2.includes("laranja");
 
-//extra não deu certo, fica a frase inteira em maiúsculo
-const gatoMaiusculo = frase2.toUpperCase(array[${frase2}"mas não deixe o gato sair"]); 
+console.log(`A frase acima contém a palavra "verde"?`,temGreen);
+console.log(`A frase acima contém a palavra "laranja"?`,temOrange);
 
-console.log (gatoMaiusculo)
+console.log(frase2.replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR"));
